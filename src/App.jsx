45f1450a6e0a5e2d73f8d4061ecd467d08a32fe4
Koushik Reddy Kambham <kkoushik_cse1905e2@mgit.ac.in>
2025-01-31 16:53:29 -0600

    createBrowserRouter/outlets

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,26 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Products from './pages/Products'
-import {Routes, Route} from 'react-router-dom'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
 import About from './pages/About'
 import Contact from './pages/Contact'
+import RootLayout from './layout/RootLayout'
 
 const App = () => {
+
+  const router=createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<RootLayout/>}>
+        <Route index element={<Home/>}></Route>
+        <Route path='products' element={<Products/>}></Route>
+        <Route path='about' element={<About/>}></Route>
+        <Route path='contact' element={<Contact/>}></Route>
+      </Route>
+    )
+  )
+
   return (
-    <div>
-      <Navbar/>
-      <div className='container'>
-        <Routes>
-          <Route path='/' element={<Home/>}></Route>
-          <Route path='/products' element={<Products/>}></Route>
-          <Route path='/about' element={<About/>}></Route>
-          <Route path='/contact' element={<Contact/>}></Route>
-        </Routes>
-      </div>
-    </div>
+    <RouterProvider router={router}></RouterProvider>
   )
 }
 
